5f6bc6d819f03f6176e0554743a244a0c04db65f
FantasyRoom <shouta_23n1100396@nnn.ed.jp>
2024-07-03 21:31:29 +0900

    template

---
+++ src/client/java/leta/multishotfishing/MultiShotFishingClient.java
@@ -0,0 +1,10 @@
+package leta.multishotfishing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiShotFishingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
