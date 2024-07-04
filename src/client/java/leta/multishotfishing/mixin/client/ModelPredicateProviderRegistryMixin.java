57879304f350e7484d9303f7682f37f5a8f381f3
shota <mine.craft.20070929@gmail.com>
2024-07-04 22:54:26 +0900

    24.07.04-2 windows to github

---
+++ src/client/java/leta/multishotfishing/mixin/client/ModelPredicateProviderRegistryMixin.java
@@ -0,0 +1,9 @@
+package leta.multishotfishing.mixin.client;
+
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ModelPredicateProviderRegistry.class)
+public class ModelPredicateProviderRegistryMixin {
+
+}
