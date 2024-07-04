1809ba8e9e189b9e2c8ec8817c3f7fb89001d3d7
FantasyRoom <shouta_23n1100396@nnn.ed.jp>
2024-07-04 19:48:39 +0900

    24.07.04-1 mac to github

--- src/main/java/leta/multishotfishing/mixin/FishingBobberEntityMixin.java
+++ src/main/java/leta/multishotfishing/mixin/FishingBobberEntityMixin.java
@@ -1,7 +1,15 @@
 package leta.multishotfishing.mixin;
 
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(FishingBobberEntityMixin.class)
+@Mixin(FishingBobberEntity.class)
 public class FishingBobberEntityMixin {
+    @Inject(method = "Lnet/minecraft/entity/projectile/FishingBobberEntity;setPlayerFishHook(Lnet/minecraft/entity/projectile/FishingBobberEntity;)V", at = @At("HEAD"), cancellable = true)
+    private void injected(CallbackInfo ci) {
+        ci.cancel();
+    }
 }
