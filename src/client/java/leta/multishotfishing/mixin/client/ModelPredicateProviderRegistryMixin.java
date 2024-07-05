77bcb559094340fe2ac774a59bc3a8f9e6449167
Leta's macbook <shouta_23n1100396@nnn.ed.jp>
2024-07-05 16:08:13 +0900

    24.07.05-0 mac to github

--- src/client/java/leta/multishotfishing/mixin/client/ModelPredicateProviderRegistryMixin.java
+++ src/client/java/leta/multishotfishing/mixin/client/ModelPredicateProviderRegistryMixin.java
@@ -1,9 +1,29 @@
 package leta.multishotfishing.mixin.client;
 
+import net.minecraft.client.item.ClampedModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FishingRodItem;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ModelPredicateProviderRegistry.class)
 public class ModelPredicateProviderRegistryMixin {
+    @ModifyArg(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/item/ModelPredicateProviderRegistry;register(Lnet/minecraft/item/Item;Lnet/minecraft/util/Identifier;Lnet/minecraft/client/item/ClampedModelPredicateProvider;)V", by = 12), index = 2)
+    private static ClampedModelPredicateProvider hogehoge(ClampedModelPredicateProvider provider) {
+        return (stack, world, entity, seed) -> {
+            if (entity == null) {
+                return 1.0F;
+            } else {
+                boolean bl = entity.getMainHandStack() == stack;
+                boolean bl2 = entity.getOffHandStack() == stack;
+                if (entity.getMainHandStack().getItem() instanceof FishingRodItem) {
+                    bl2 = false;
+                }
 
+                return (bl || bl2) && entity instanceof PlayerEntity && ((PlayerEntity)entity).fishHook != null ? 0.0F : 1.0F;
+            }
+        };
+    }
 }
